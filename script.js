fae5951b518b434ce270d5cd446def43c188f950
Sahil Bajaj <sahil29@gmail.com>
2013-02-19 20:45:15 +0100

    Improve code
    * Added comments
    * Handled lyrics not being found and instrumental songs
    * Using LyricWiki API's XML result now
    * Using cheerio only to get lyrics from actual lyrics page (not for the
      API response)

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
-//console.log(process.argv)
 var request = require('request')
 var cheerio = require('cheerio')
 
 process.stdin.resume();
-// process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf8');
 
+// Get data from stdin (pipe artist name and song title from
+// AppleScript)
 process.stdin.on('data', function(data) {
-	// process.stdout.write(data);
-	data = (data + "").split(' - ')
-	var artist = data[0]
-	var song = data[1]
-	request.get(encodeURI('http://lyrics.wikia.com/api.php?artist=' + artist + '&song=' + song), function (error, response, body) {
-		var $ = cheerio.load(body)
-		var url = $('a[title="url"]').text()
 
-		request.get(url, function (error, response, body) {
+	// Check that an artist and song were passed in
+	if (data.indexOf(' - ') === -1) {
+		process.stdout.write('That does not seem to be a song.\n')
+		return
+	}
+
+	// Separate artist and song title
+	data = data.replace(/\n/, '').split(' - ')
+	var queryUrl = 'http://lyrics.wikia.com/api.php?artist=' + data[0] + '&song=' + data[1] + '&fmt=xml'
+
+	// Query using LyricWiki's API
+	request.get(encodeURI(queryUrl), function (error, response, body) {
+
+		var matches
+
+		// Get artist and song information
+		matches = body.match(/<artist>(.+)<\/artist>/)
+		var artist = matches && matches[1]
+
+		matches = body.match(/<song>(.+)<\/song>/)
+		var song = matches && matches[1]
+
+		// Check if lyrics were found
+		if (body.indexOf('<lyrics>Not found') !== -1) {
+			process.stdout.write('No lyrics could be found for ' + song + ' by ' + artist + '.\n')
+			return
+		}
+
+		// Check if song is an istrumental piece
+		if (body.indexOf('<lyrics>Instrumental') !== -1) {
+			process.stdout.write(song + ' by ' + artist + ' is an instrumental.\n')
+			return
+		}
+
+		// Get actual URL to lyrics
+		matches = body.match(/<url>(.+)<\/url>/)
+		var lyricsUrl = matches && matches[1]
+
+		if (!lyricsUrl) {
+			process.stdout.write('Invalid lyrics URL.\n')
+			return
+		}
+
+		// Fetch the lyrics page and get the actual lyrics from it
+		request.get(lyricsUrl, function (error, response, body) {
+
 			var $ = cheerio.load(body)
+
+			// Remove extra non-lyrics text
 			$('.lyricbox').find('div').remove()
-			// $('.lyricbox').find('.lyricsbreak').remove()
-			// var lyrics = $('.lyricbox').text()
 			var html = $('.lyricbox').html()
 			html = html.substring(0, html.indexOf('<!--'))
 
 			var $div = $('<div></div>')
 			$div.text(html.replace(/<br>/gi, '\n'))
 
+			process.stdout.write(song + ' by ' + artist + '\n\n')
 			process.stdout.write($div.text())
 		})
 	})
